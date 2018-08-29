8dc22f17196d296a19a194cf2f454097ab076097
Anu_Rolly <anushabiswas27@gmail.com>
2018-08-30 02:20:30 +1000

    feat : filtering and stuff

--- js/googleChart_barchart_industry.js
+++ js/googleChart_barchart_industry.js
@@ -1,25 +1,97 @@
-google.charts.load('current', {'packages':['bar']});
+google.charts.load('current', {
+  packages: ['corechart', 'bar']
+});
 //google.charts.setOnLoadCallback(drawChart);
 
 function chart2() {
-  var data = google.visualization.arrayToDataTable(changeOccupationData());
+
+  var data = google.visualization.arrayToDataTable(changeIndustryData());
 
   var options = {
     chart: {
       title: 'Hours Worked by Employees',
-      subtitle: 'Yearly and Industry',
+      subtitle: 'Yearly and Occupation',
     },
-    animation:{
-      duration: 1500,
+    animation: {
+      duration: 500,
       startup: true
-        },
+    },
     bars: 'vertical',
+    bar: {
+      groupWidth: "95%"
+    },
+    vAxis: {
+      title: "Number of Employees"
+    },
+    hAxis: {
+      title: "Industry",
+      slantedText: false,
+      showTextEvery: 1,
+      textStyle: {
+        fontSize: 10
+      }
+    },
+    legend: {
+      position: "none"
+    },
     height: 450,
-    colors: ['#006203', '	#0f9200', '#30cb00' ,'#ff7e7e', '#ff6969', '#ff5353', '#ff3b3b']
+    width: '100%',
+    colors: barColorArrayIndustry(),
+    //chartArea: {width: '98%', height: '98%'}
+    chartArea: {
+      left: 90,
+      top: 10,
+      width: '90%',
+      height: '85%'
+    }
   };
 
-  var chart = new google.charts.Bar(document.getElementById('chart_div_2'));
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div_2'));
 
   chart.draw(data, options);
-  }
-  
+}
+
+function chartFilteredIndustry() {
+
+  var data = google.visualization.arrayToDataTable(funcchangedData());
+
+  var options = {
+    chart: {
+      title: 'Hours Worked by Employees',
+      subtitle: 'Yearly and Occupation',
+    },
+    animation: {
+      duration: 500,
+      startup: true
+    },
+    bars: 'vertical',
+    bar: {
+      groupWidth: "95%"
+    },
+    vAxis: {
+      title: "Number of Employees"
+    },
+    hAxis: {
+      title: "Occupation",
+      slantedText: false,
+      showTextEvery: 1,
+      maxTextLines: 4
+    },
+    legend: {
+      position: "none"
+    },
+    height: 450,
+    colors: changedColorI,
+    //chartArea: {width: '98%', height: '98%'}
+    chartArea: {
+      left: 90,
+      top: 10,
+      width: '90%',
+      height: '85%'
+    }
+  };
+
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div_2'));
+
+  chart.draw(data, options);
+}
--- js/googleChart_barchart_occupation.js
+++ js/googleChart_barchart_occupation.js
@@ -1,27 +1,96 @@
-google.charts.load('current', {'packages':['bar']});
+google.charts.load('current', {
+  packages: ['corechart', 'bar']
+});
 //google.charts.setOnLoadCallback(drawChart);
 
 function chart1() {
-  
-    var data = google.visualization.arrayToDataTable(changeIndustryData());
+
+  var data = google.visualization.arrayToDataTable(changeOccupationData());
 
   var options = {
     chart: {
       title: 'Hours Worked by Employees',
       subtitle: 'Yearly and Occupation',
     },
-    animation:{
-      duration: 1500,
+    animation: {
+      duration: 500,
       startup: true
-        },
+    },
     bars: 'vertical',
-    vAxis: {title : "employee number"},
-    hAxis : {title : "Occupation" , slantedText:true},
+    bar: {
+      groupWidth: "95%"
+    },
+    vAxis: {
+      title: "Number of Employees"
+    },
+    hAxis: {
+      title: "Occupation",
+      slantedText: false,
+      showTextEvery: 1,
+      maxTextLines: 4
+    },
+    legend: {
+      position: "none"
+    },
     height: 450,
-    colors: ['#006203', '	#0f9200', '#30cb00' ,'#ff7e7e', '#ff6969', '#ff5353', '#ff3b3b']
+    width: '100%',
+    colors: barColorArray(),
+    //chartArea: {width: '98%', height: '98%'}
+    chartArea: {
+      left: 90,
+      top: 10,
+      width: '90%',
+      height: '85%'
+    }
   };
 
-  var chart = new google.charts.Bar(document.getElementById('chart_div_1'));
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div_1'));
 
   chart.draw(data, options);
-  }
+}
+
+function chartFiltered() {
+
+  var data = google.visualization.arrayToDataTable(funcchangedData());
+
+  var options = {
+    chart: {
+      title: 'Hours Worked by Employees',
+      subtitle: 'Yearly and Occupation',
+    },
+    animation: {
+      duration: 500,
+      startup: true
+    },
+    bars: 'vertical',
+    bar: {
+      groupWidth: "95%"
+    },
+    vAxis: {
+      title: "Number of Employees"
+    },
+    hAxis: {
+      title: "Occupation",
+      slantedText: false,
+      showTextEvery: 1,
+      maxTextLines: 4
+    },
+    legend: {
+      position: "none"
+    },
+    height: 450,
+    width: '100%',
+    colors: changedColorO,
+    //chartArea: {width: '98%', height: '98%'}
+    chartArea: {
+      left: 90,
+      top: 10,
+      width: '90%',
+      height: '85%'
+    }
+  };
+
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div_2'));
+
+  chart.draw(data, options);
+}
